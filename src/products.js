6254c5d1addbda5a2e9f7e97a942ed4b61af2121
rohan saldanha <rohansaldanha.int@niveussolutions.com>
2023-04-05 17:41:29 +0530

    first committed

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.jpg";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.jpg";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.jpg";
+import product7 from "./assets/products/7.jpg";
+import product8 from "./assets/products/8.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Yellow African Ethnic",
+    price: 149.99,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Army Green Dress",
+    price: 119.99,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Muddy Brown Jacket",
+    price: 174.99,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "WLS Van Gogh Denim Dress",
+    price: 99.99,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "US Polo Free Wear",
+    price: 108.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "D&G Pant",
+    price: 76.99,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Yellow Indian Ethnic Wear",
+    price: 129.99,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Romanian Jet Black Dress",
+    price: 202.98,
+    productImage: product8,
+  },
+];

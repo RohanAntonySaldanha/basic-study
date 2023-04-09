5ae54f71f56f3c926579a9b227356049925f1101
rohan saldanha <rohansaldanha.int@niveussolutions.com>
2023-04-09 13:10:04 +0530

    sidebar cancel

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-// import "./App.css";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { Navbar } from "./components/navbar";
-// import { Shop } from "./pages/shop/shop";
-// import { Contact } from "./pages/contact";
-// import { Cart } from "./pages/cart/cart";
-// import { ShopContextProvider } from "./context/shop-context";
-// import SideNavBar from "./SideNavBar/SideNavBar";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <ShopContextProvider>
-//         <SideNavBar />
-//         <Router>
-//           <Navbar />
-          
-//             <Routes>
-//               <Route path="/" element={<Shop />} />
-//               <Route path="/contact" element={<Contact />} />
-//               <Route path="/cart" element={<Cart />} />
-//             </Routes>
-//         </Router>
-//       </ShopContextProvider>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
 
-// export default App;
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+    </div>
+  );
+}
 
-
+export default App;

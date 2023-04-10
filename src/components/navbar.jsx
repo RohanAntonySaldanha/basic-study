359117379821c9059d260ef7047845f7d9ae3a53
rohan saldanha <rohansaldanha.int@niveussolutions.com>
2023-04-10 10:02:29 +0530

    navbar updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ShoppingCart ,Storefront} from "phosphor-react";
+import { ShoppingCart, Storefront } from "phosphor-react";
 import "./navbar.css";
 
 export const Navbar = () => {
@@ -11,13 +11,17 @@ export const Navbar = () => {
           <Storefront size={40} />
         </Link>
       </div>
-      <div className="navtitle"><h1>RohanShop.com</h1></div>
+      <div className="navtitle">
+        <h1>RohanShop.com</h1>
+      </div>
       <div className="linkscart">
         <Link to="/cart">
           <ShoppingCart size={32} />
+        </Link>
+        <Link to="/">
+          <b>Cart</b>
         </Link>
       </div>
     </div>
   );
 };
-
